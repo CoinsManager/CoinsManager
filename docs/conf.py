f036676f9b54135229a0c19fbdca2758183c2eba
Adrien Lemaire <lemaire.adrien@gmail.com>
2013-12-17 15:53:03 +1100

    add several logo sizes

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../public/logo/logo_mvp_1.png'
+html_logo = '../public/logo/logo_mvp_1-128x128.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
