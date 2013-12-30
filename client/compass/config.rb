5c95aa8cf2671ae02e7ea6a3b2591e8d8500b456
Vladimir Plokhov <vladimirplokhov@gmail.com>
2013-12-30 19:36:26 +0100

    Fix in config.rb for location of public files

--- client/compass/config.rb
+++ client/compass/config.rb
@@ -5,14 +5,18 @@ require 'breakpoint'
 http_path = "/"
 css_dir = "../css"
 sass_dir = "sass"
-images_dir = "../img"
-fonts_dir = "../fonts/"
+images_dir = "../../public/img"
+fonts_dir = "../../public/fonts"
+
+# Deployed paths:
+http_generated_images_path = "/img"
+http_fonts_path = "/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+# relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
